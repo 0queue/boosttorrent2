3238c504a5a30401207f167377dfb78f3ebdb292
Thomas Harris <teh019283@gmail.com>
2018-10-25 12:53:14 -0400

    [boostencode] Move parsing functions to own submodule
    Top level boostencode should just have types and impls

---
+++ src/boostencode/parse/mod.rs
@@ -0,0 +1,119 @@
+use boostencode::compare_bstring;
+use std::cmp::Ordering;
+use std::collections::HashMap;
+use super::Value;
+
+#[cfg(test)]
+mod test;
+
+pub fn parse_val(bytes: &mut Vec<u8>) -> Value {
+    match bytes[0] as char {
+        'i' => parse_integer(bytes),
+        'l' => parse_list(bytes),
+        'd' => parse_dict(bytes),
+        '0'...'9' => parse_bstring(bytes),
+        _ => panic!("Not bencode!")
+    }
+}
+
+// assured of a bstring, we take it off the stack of bytes and return it
+fn parse_bstring(bytes: &mut Vec<u8>) -> Value {
+    let len = parse_integer_literal(bytes);
+    if bytes.remove(0) as char != ':' {
+        panic!("error: expected ':'");
+    }
+
+    let bstring: Vec<u8> = bytes.drain(0..len).collect();
+
+    Value::BString(bstring)
+}
+
+fn parse_integer(bytes: &mut Vec<u8>) -> Value {
+    if bytes.remove(0) as char != 'i' {
+        panic!("expected i to prefix integer");
+    }
+
+    let is_negative = bytes[0] as char == '-';
+    if is_negative {
+        bytes.remove(0);
+    }
+
+    if bytes[0] as char == '0' {
+        if is_negative || bytes[1] as char != 'e' {
+            panic!("integer has leading or negative zero");
+        }
+
+        return Value::Integer(0);
+    }
+
+    let num = parse_integer_literal(bytes) as i32;
+
+    if bytes.remove(0) as char != 'e' {
+        panic!("expected e after integer");
+    }
+
+    Value::Integer(if is_negative { -num } else { num })
+}
+
+fn parse_list(bytes: &mut Vec<u8>) -> Value {
+    let mut list = Vec::new();
+    if bytes.remove(0) as char != 'l' {
+        panic!("expected list prefix");
+    }
+
+    while bytes[0] as char != 'e' {
+        list.push(parse_val(bytes))
+    }
+
+    if bytes.remove(0) as char != 'e' {
+        panic!("Expected list postfix");
+    }
+
+    Value::List(list)
+}
+
+fn parse_dict(bytes: &mut Vec<u8>) -> Value {
+    let mut map = HashMap::new();
+
+    if bytes.remove(0) as char != 'd' {
+        panic!("expected dict prefix");
+    }
+
+    let mut last_key: Option<Vec<u8>> = None;
+
+    while bytes[0] as char != 'e' {
+        let key = parse_bstring(bytes);
+        let val = parse_val(bytes);
+
+        if let Value::BString(key) = key {
+            if let Some(last) = last_key {
+                if compare_bstring(&last, key.as_ref()) != Ordering::Less {
+                    panic!("dict keys not in ascending order");
+                }
+            }
+
+            last_key = Some(key.clone());
+            map.insert(key, val);
+        } else {
+            panic!("Key is not BString");
+        }
+    }
+
+    if bytes.remove(0) as char != 'e' {
+        panic!("expected dict postfix");
+    }
+
+    Value::Dict(map)
+}
+
+// parse an integer literal on the top of the stack
+fn parse_integer_literal(bytes: &mut Vec<u8>) -> usize {
+    let mut num = String::new();
+    while bytes.len() > 0 && bytes[0] as char >= '0' && bytes[0] as char <= '9' {
+        num.push(bytes.remove(0) as char);
+    }
+
+    // TODO better errors
+
+    num.parse().unwrap()
+}
---
+++ src/boostencode/parse/test.rs
@@ -0,0 +1,76 @@
+use super::*;
+
+#[test]
+fn test_parse_integer_literal() {
+    let mut s123 = "123e".to_string().into_bytes();
+    let res = parse_integer_literal(s123.as_mut());
+    assert_eq!(res, 123);
+}
+
+#[test]
+fn test_parse_bstring() {
+    let mut s1 = "4:spam".to_string().into_bytes();
+
+    let val = parse_bstring(s1.as_mut());
+
+    assert_eq!(val, Value::BString(vec!['s' as u8, 'p' as u8, 'a' as u8, 'm' as u8]));
+    assert_eq!(0, s1.len());
+}
+
+#[test]
+fn test_parse_integer() {
+    let mut s1 = "i123e".to_string().into_bytes();
+    let mut s2 = "i-4e".to_string().into_bytes();
+
+    let val1 = parse_integer(s1.as_mut());
+    let val2 = parse_integer(s2.as_mut());
+
+    assert_eq!(val1, Value::Integer(123));
+    assert_eq!(val2, Value::Integer(-4));
+    assert_eq!(0, s1.len());
+    assert_eq!(0, s2.len());
+}
+
+#[test]
+#[should_panic]
+fn test_parse_integer_negative_zero() {
+    let mut s1 = "i-0e".to_string().into_bytes();
+
+    let _val1 = parse_integer(s1.as_mut());
+}
+
+#[test]
+#[should_panic]
+fn test_parse_integer_leading_zero() {
+    let mut s1 = "i023e".to_string().into_bytes();
+
+    let _val1 = parse_integer(s1.as_mut());
+}
+
+#[test]
+fn test_parse_list() {
+    let mut s1 = "l4:spami123ee".to_string().into_bytes();
+
+    let val1 = parse_list(s1.as_mut());
+
+    assert_eq!(val1, Value::List(vec![Value::BString(vec!['s' as u8, 'p' as u8, 'a' as u8, 'm' as u8]), Value::Integer(123)]))
+}
+
+#[test]
+fn test_parses_dict() {
+    let mut s1 = "d5:hello5:world4:spami123ee".to_string().into_bytes();
+    let val1 = parse_dict(s1.as_mut());
+
+    let mut map = HashMap::new();
+    map.insert(vec!['h' as u8, 'e' as u8, 'l' as u8, 'l' as u8, 'o' as u8], Value::BString(vec!['w' as u8, 'o' as u8, 'r' as u8, 'l' as u8, 'd' as u8]));
+    map.insert(vec!['s' as u8, 'p' as u8, 'a' as u8, 'm' as u8], Value::Integer(123));
+    assert_eq!(val1, Value::Dict(map));
+}
+
+#[test]
+#[should_panic]
+fn test_parse_dict_not_ascending() {
+    let mut s1 = "d5:worldi1e5:helloi2ee".to_string().into_bytes();
+
+    let _val1 = parse_dict(s1.as_mut());
+}
